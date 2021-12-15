c4c387513cde7d062338870b3da0b398235d96d4
PandA <1554296563@qq.com>
2021-12-15 10:16:39 +0800

    deploy

---
+++ precache-manifest.6880f00bdda15adf7913a0405896ba37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6f497bf24757f0eb21",
+    "url": "/money/css/app.68d960bc.css"
+  },
+  {
+    "revision": "afa04f5a3a41df6a3637c32ae08876dd",
+    "url": "/money/index.html"
+  },
+  {
+    "revision": "6a6f497bf24757f0eb21",
+    "url": "/money/js/app.5cc09433.js"
+  },
+  {
+    "revision": "461743fffe059b8cf48e",
+    "url": "/money/js/chunk-vendors.e2b2e8f1.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/money/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money/robots.txt"
+  }
+]);
